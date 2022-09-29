3f5b5b0b1380eb4cabdb12447366f2e88607c9f6
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-09-29 16:48:12 +0700

    add Route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllUser } from './service/userService'
-function App() {
-  const listUser = useSelector(state => state.userReducer.users)
-  console.log(listUser);
-  const dispatch = useDispatch()
-  const handelClick = () => {
-    getAllUser(dispatch)
-  }
-  return (
-    <div>
-      <button onClick={handelClick}>Click</button>
-      {listUser && <div>
-        {listUser.map((user)=>(
-          <li>{user.name}</li>
-        ))}
-      </div>}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-    </div>
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login"></Route>
+        <Route path="/register"></Route>
+        <Route path="/home"></Route>
+        <Route path="/admin"></Route>
+        <Route path="/seller"></Route>
+        <Route path="/accountant"></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
