263b4271e1edc97b968451aa8d2e0d52c29bee23
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-09-29 20:40:49 +0700

    add service

--- src/reducer/slice/userSlice.js
+++ src/reducer/slice/userSlice.js
@@ -1,17 +1,52 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     users: []
 }
 const userSlice = createSlice({
-    name:'users',
+    name: 'users',
     initialState,
-    reducers:{
-        getAllUserSlice: (state,action) =>{
+    reducers: {
+        getAllStaffSlice:(state,action) => {
+            
+        },
+        getAllUserSlice: (state, action) => {
             state.users = action.payload
+        },
+        getDetailUserSlice:(state, action) => {
+
+        },
+        addStaffSlice:(state, action) => {
+            
+        },
+        deleteUserSlice:(state, action) => {
+            
+        },
+        updateRoleUserSlice:(state, action) => {
+            
+        },
+        updateUserSlice:(state, action) => {
+            
+        },
+        searchUserSlice:(state, action) => {
+            
+        },
+        searchStaffSlice:(state, action) => {
+            
         }
     }
 })
 
-export const {getAllUserSlice} = userSlice.actions;
+export const {
+    getAllUserSlice,
+    getAllStaffSlice,
+    getDetailUserSlice,
+    addStaffSlice,
+    deleteUserSlice,
+    updateRoleUserSlice,
+    updateUserSlice,
+    searchUserSlice,
+    searchStaffSlice
+} = userSlice.actions;
+
 export default userSlice.reducer;
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,9 +1,52 @@
 
-import {customAxios} from "./tokenHeader";
-import { getAllUserSlice } from "../reducer/slice/userSlice";
+import { customAxios } from "./tokenHeader";
+import {
+    getAllUserSlice,
+    getAllStaffSlice,
+    getDetailUserSlice,
+    addStaffSlice,
+    deleteUserSlice,
+    updateRoleUserSlice,
+    updateUserSlice,
+    searchUserSlice,
+    searchStaffSlice
+} from "../reducer/slice/userSlice";
+
 const baseURL = "https://localhost:3000/";
 
 export const getAllUser = async (dispatch) => {
-    const res = await customAxios.get('http://localhost:3000/user')
+    const res = await customAxios.get(`${baseURL}/user`)
     dispatch(getAllUserSlice(res.data))
+}
+export const getAllStaff = async (dispatch) => {
+    const res = await customAxios.get(`${baseURL}/user/staffs`)
+    dispatch(getAllStaffSlice(res.data))
+}
+export const getDetailUser = async (id, dispatch) => {
+    const res = await customAxios.get(`${baseURL}/user/${id}`)
+    dispatch(getDetailUserSlice(res.data))
+}
+export const addStaff = async (dispatch) => {
+    const res = await customAxios.post(`${baseURL}/user`)
+    dispatch(addStaffSlice(res.data))
+}
+export const deleteUser = async (id, dispatch) => {
+    const res = await customAxios.delete(`${baseURL}/user/${id}`)
+    dispatch(deleteUserSlice(res.data))
+}
+export const updateRoleUser = async (id, dispatch) => {
+    const res = await customAxios.put(`${baseURL}/user/updateRoleUser/${id}`)
+    dispatch(updateRoleUserSlice(res.data))
+}
+export const updateUser = async (id, dispatch) => {
+    const res = await customAxios.put(`${baseURL}/user/${id}`)
+    dispatch(updateUserSlice(res.data))
+}
+export const searchUser = async (data, dispatch) => {
+    const res = await customAxios.put(`${baseURL}/user/searchUser,${data}`)
+    dispatch(searchUserSlice(res.data))
+}
+export const searchStaff = async (data, dispatch) => {
+    const res = await customAxios.put(`${baseURL}/user/searchStaff,${data}`)
+    dispatch(searchStaffSlice(res.data))
 }
